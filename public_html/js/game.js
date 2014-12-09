256c08f9e46aa28270578994f2b97a4d60bd7a8b
castillorosy <rosycastillo18@yahoo.com>
2014-12-08 22:27:08 -0800

    nm'lkn's'kla

--- public_html/js/game.js
+++ public_html/js/game.js
@@ -1,28 +1,24 @@
-
 /* Game namespace */
 var game = {
+    // an object where to store game information
+    data: {
+        // score
+        score: 0
+   },
+    // Run on page load.
+    "onload": function() {
+        // Initialize the video.
+        if (!me.video.init("screen", me.video.CANVAS, 1067, 600, true, 1.0)) {
+            alert("Your browser does not support HTML5 canvas.");
+            return;
+        }
 
-	// an object where to store game information
-	data : {
-		// score
-		score : 0
-	},
-	
-	
-	// Run on page load.
-	"onload" : function () {
-	// Initialize the video.
-	if (!me.video.init("screen",  me.video.CANVAS, 1067, 600, true, 1.0)) {
-		alert("Your browser does not support HTML5 canvas.");
-		return;
-	}
-
-	// add "#debug" to the URL to enable the debug Panel
-	if (document.location.hash === "#debug") {
-		window.onReady(function () {
-			me.plugin.register.defer(this, debugPanel, "debug");
-		});
-	}
+        // add "#debug" to the URL to enable the debug Panel
+        if (document.location.hash === "#debug") {
+            window.onReady(function() {
+                me.plugin.register.defer(this, debugPanel, "debug");
+            });
+        }
 
 	// Initialize the audio.
 	me.audio.init("mp3,ogg");
