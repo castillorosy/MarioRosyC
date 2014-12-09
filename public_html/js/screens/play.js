804801ac4f6adb31cd700d20d036ca68a09274ac
castillorosy <rosycastillo18@yahoo.com>
2014-12-09 13:47:43 -0800

    blank

--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -5,12 +5,12 @@ game.PlayScreen = me.ScreenObject.extend({
     onResetEvent: function() {
         // reset the score
         game.data.score = 0;
-
+//helps get into the next level of mario and tells it when the level goes
         me.levelDirector.loadLevel("level04");
-
+//how big mario is suppose to be or anything else
         var player = me.pool.pull("mario", 0, 420, {});
         me.game.world.addChild(player, 3);
-
+//helps mario moves 
         me.input.bindKey(me.input.KEY.RIGHT, "right");
 
         // add our HUD to the game world
